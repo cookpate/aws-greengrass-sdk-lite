f3ad48de8bc873dc2f6a2d689b971aa243fdf1f1
Archit Gupta <archigup@amazon.com>
2025-04-23 09:22:39 -0700

    Merge modules needed for IPC client into ggl-sdk

---
+++ priv_include/ggl/attr.h
@@ -0,0 +1,28 @@
+// aws-greengrass-lite - AWS IoT Greengrass runtime for constrained devices
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef GGL_ATTR_H
+#define GGL_ATTR_H
+
+#ifdef __has_attribute
+
+#if __has_attribute(counted_by)
+#define COUNTED_BY(field) __attribute__((counted_by(field)))
+#endif
+
+#if __has_attribute(designated_init)
+#define DESIGNATED_INIT __attribute__((designated_init))
+#endif
+
+#endif // __has_attribute
+
+#ifndef COUNTED_BY
+#define COUNTED_BY(field)
+#endif
+
+#ifndef DESIGNATED_INIT
+#define DESIGNATED_INIT
+#endif
+
+#endif
