b6059d4151927a95a60fce82003cde1abf55c8be
Archit Gupta <archigup@amazon.com>
2025-07-02 10:28:27 -0700

    Use GCC CRC builtin if available

--- src/crc32.c
+++ src/crc32.c
@@ -7,6 +7,24 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#ifdef __has_builtin
+#if __has_builtin(__builtin_rev_crc32_data8)
+#define HAS_BUILTIN_CRC 1
+#endif
+#endif
+
+#ifndef HAS_BUILTIN_CRC
+#define HAS_BUILTIN_CRC 0
+#endif
+
+#if HAS_BUILTIN_CRC
+
+static uint32_t crc_step(uint32_t crc, uint8_t byte) {
+    return __builtin_rev_crc32_data8(crc, byte, 0x04C11DB7L)
+}
+
+#else
+
 // CRC code adapted from rfc1952 GZIP file format specification version 4.3
 
 /// Table of CRCs of all 8-bit messages.
@@ -28,10 +46,16 @@ __attribute__((constructor)) static void make_crc_table(void) {
     }
 }
 
+static uint32_t crc_step(uint32_t crc, uint8_t byte) {
+    return crc_table[(crc ^ byte) & 0xFF] ^ (crc >> 8);
+}
+
+#endif
+
 uint32_t ggl_update_crc(uint32_t crc, GglBuffer buf) {
     uint32_t c = ~crc;
     for (size_t n = 0; n < buf.len; n++) {
-        c = crc_table[(c ^ buf.data[n]) & 0xFF] ^ (c >> 8);
+        c = crc_step(c, buf.data[n]);
     }
     return ~c;
 }
